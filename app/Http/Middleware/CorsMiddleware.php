b72f041aba6dc35b4bc42760898c8f2bb5e2a530
EiEiLwinDev <eieilwin.dev@gmail.com>
2024-05-26 16:59:35 +0700

    save

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,7 +17,7 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', env('FRONTEND_URL', 'http://157.245.199.178'));
+        $response->headers->set('Access-Control-Allow-Origin', env('FRONTEND_URL', 'http://152.42.236.196'));
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,12 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \App\Http\Middleware\CorsMiddleware::class
         ]);
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-            'cors' => \App\Http\Middleware\CorsMiddleware::class
         ]);
 
         //
